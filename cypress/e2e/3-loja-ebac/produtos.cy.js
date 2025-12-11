13b70b27929aa1ced7f44a5c52a5b75fa6ee47dd
Luiz Felipe <luizfelipemedeiros09@gmail.com>
2025-12-10 21:29:15 -0300

    Massa de Dados em Lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,16 +13,31 @@ describe('Funcionalidade: Produtos', () => {
         
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         produtosPage.buscarProdutos('Aether Gym Pant')
         cy.get('.product_title').should('contain' , 'Aether Gym Pant')
     });
 
     it('Deve visitar a pagina do produto', () => {
-        
+        produtosPage.visitarProduto('Argus All-Weather Tank')
+        cy.get('.product_title').should('contain' , 'Argus All-Weather Tank')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
-    });
+        let qtd = 5
+        produtosPage.buscarProdutos('Cassia Funnel Sweatshirt')
+        produtosPage.addProdutoCarrinho('XS', 'Orange', qtd)
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Cassia Funnel Sweatshirt” foram adicionados no seu carrinho.')
+    })
+
+    it.only('Deve adicionar produto ao carrinho com Massa de Dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProdutos(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+    })
 });
